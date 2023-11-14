f390820c229c9b8aa8373fe53cd7df9f4b7b8faf
Vinícius da Silva Dias <vinicius.dias@triasoftware.com.br>
2023-11-13 21:09:29 -0300

    k k k

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 
 @NgModule({
